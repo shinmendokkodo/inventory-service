7010fd8950dce54e7dc6eb18d5659aa58329e443
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-04-20 01:33:33 +0530

    Initial commit

---
+++ src/main/java/io/shinmen/beerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package io.shinmen.beerinventoryservice.web.mappers;
+
+import io.shinmen.beerinventoryservice.domain.BeerInventory;
+import io.shinmen.beerinventoryservice.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
